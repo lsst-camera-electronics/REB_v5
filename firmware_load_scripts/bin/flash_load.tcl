2537e367fc53430b46e0597008a1d8bb7595a555
Gregg Thayer <jgt@slac.stanford.edu>
2021-09-14 08:18:30 -0700

    Changed flash_load to erase entire EEPROM prior to burning mcs file.

--- firmware_load_scripts/bin/flash_load.tcl
+++ firmware_load_scripts/bin/flash_load.tcl
@@ -13,27 +13,27 @@ current_hw_target [get_hw_targets]
 #this is for the digilent module
 set_property PARAM.FREQUENCY 30000000 [get_hw_targets]
 
+
 open_hw_target
+
 
 current_hw_device [lindex [get_hw_devices] 0]
 
 create_hw_cfgmem -hw_device [lindex [get_hw_devices] 0] -mem_dev  [lindex [get_cfgmem_parts {n25q256-3.3v-spi-x1_x2_x4}] 0]
 
 
-set_property PROGRAM.BLANK_CHECK  0 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0 ]]
+set_property PROGRAM.BLANK_CHECK  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0 ]]
 set_property PROGRAM.ERASE  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0 ]]
 set_property PROGRAM.CFG_PROGRAM  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0 ]]
 set_property PROGRAM.VERIFY  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0 ]]
 set_property PROGRAM.CHECKSUM  0 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0 ]]
 refresh_hw_device [lindex [get_hw_devices] 0]
-set_property PROGRAM.ADDRESS_RANGE  {use_file} [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0 ]]
+set_property PROGRAM.ADDRESS_RANGE  {entire_device} [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0 ]]
 
-#set_property PROGRAM.FILES ./[lindex $argv 0]  [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0]]
-
-set_property PROGRAM.FILES [lindex $argv 0]  [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0]]
+set_property PROGRAM.FILES ./[lindex $argv 0]  [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0]]
 
 set_property PROGRAM.UNUSED_PIN_TERMINATION {pull-none} [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0 ]]
-set_property PROGRAM.BLANK_CHECK  0 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0 ]]
+set_property PROGRAM.BLANK_CHECK  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0 ]]
 set_property PROGRAM.ERASE  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0 ]]
 set_property PROGRAM.CFG_PROGRAM  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0 ]]
 set_property PROGRAM.VERIFY  1 [ get_property PROGRAM.HW_CFGMEM [lindex [get_hw_devices] 0 ]]
@@ -45,8 +45,7 @@ program_hw_cfgmem -hw_cfgmem [get_property PROGRAM.HW_CFGMEM [lindex [get_hw_dev
 endgroup
 boot_hw_device  [lindex [get_hw_devices] 0]
 
+
 close_hw_target
-
 disconnect_hw_server localhost:3121
-
 close_hw
